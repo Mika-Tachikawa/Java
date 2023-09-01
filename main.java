cd07c84997f6e523e848463e9da575d164324bc7
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-09-01 02:06:45 +0000

    javaでのwhile文実行練習

---
+++ main.java
@@ -0,0 +1,11 @@
+ // while文を用いて、numberが0より大きい場合に繰り返す、繰り返し処理
+
+class Main {
+  public static void main(String[] args) {
+    int number = 10;
+      while(number > 0){
+        System.out.println(number);
+        number--;
+      }
+  }
+}
