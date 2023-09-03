6254c9f6f5ba97f1fb497138f82a137d02917651
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-09-03 05:51:31 +0000

    elseの追加

--- Ifsample.java
+++ Ifsample.java
@@ -4,8 +4,10 @@ public class Ifsample {
   
   public static void main(String[] args) {
     var a =2;
-    if(a<3){
+    if(a>3){
       System.out.println("小さい");
+    }else{
+      System.out.println("大きい");
     }
   }
   
