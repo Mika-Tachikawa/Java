452509ea4f001c44ae69607efc63a80fe07d8e5b
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-09-04 01:03:55 +0000

    Forsampleファイルのコンパイル

--- Forsample.java
+++ Forsample.java
@@ -2,8 +2,8 @@ package test55;
 
 public class Forsample {
   public static void main(String[] args) {
-    for (int i=0; i<5; i+=){
+    for (int i=0; i<5; i+=2){
       System.out.println(i);
-    }  
+    }
   }
 }
