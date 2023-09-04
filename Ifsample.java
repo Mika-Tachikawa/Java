3bf42404ee4a34ab426081d93ac4649086f640cf
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-09-04 00:40:49 +0000

    elseifの追記

--- Ifsample.java
+++ Ifsample.java
@@ -3,9 +3,11 @@ package test55;
 public class Ifsample {
   
   public static void main(String[] args) {
-    var a =2;
-    if(a>3){
+    var a = 5;
+    if(a < 3){
       System.out.println("小さい");
+    }else if(a < 7){
+      System.out.println("中くらい");
     }else{
       System.out.println("大きい");
     }
