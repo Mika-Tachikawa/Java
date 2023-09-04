1d0a814b8fbab965ac1a0a73a7e9d46c0dbf97fe
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-09-04 01:05:59 +0000

    Forsampleファイルの修正

--- Forsample.java
+++ Forsample.java
@@ -2,7 +2,7 @@ package test55;
 
 public class Forsample {
   public static void main(String[] args) {
-    for (int i=0; i<5; i+=2){
+    for (int i=3; i>0; i--){
       System.out.println(i);
     }
   }
