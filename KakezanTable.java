248ea11857d666e7330791f333ba16fcc5a1da59
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-09-05 01:15:01 +0000

    二重ループのコード追記

---
+++ KakezanTable.java
@@ -0,0 +1,12 @@
+package test55;
+
+public class KakezanTable {
+  public static void main(String[] args) {
+    for (int i=1; i<=5; i++){
+      for (int j=1; j<=9; i++){
+        System.out.printf("%2d|",i*j);
+      }
+    System.out.prinln();
+    }
+  }
+}
