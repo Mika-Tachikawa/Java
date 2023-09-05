d62b7a68c8ca72ba98f092b0f9a50ba6b9df6382
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-09-05 01:20:52 +0000

    二重ループのコードの修正

--- KakezanTable.java
+++ KakezanTable.java
@@ -3,7 +3,7 @@ package test55;
 public class KakezanTable {
   public static void main(String[] args) {
     for (int i=1; i<=5; i++){
-      for (int j=1; j<=9; i++){
+      for (int j=1; j<=9; j++){
         System.out.printf("%2d|",i*j);
       }
     System.out.println();
