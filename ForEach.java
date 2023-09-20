b84bfe666161b596c26eb4631fb2dae41a304b70
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-09-20 03:25:18 +0000

    基本for文の記述追加

---
+++ ForEach.java
@@ -0,0 +1,14 @@
+package projava;
+
+import java.util.List;
+
+public class ForEach {
+  
+  public static void main(String[] args){
+    var strs = List.of("apple","banana","grape");
+    for (int i=0; i<strs.size(); i++){
+      var strs.get(i);
+      System.out.println(str);
+    }
+  }
+}
