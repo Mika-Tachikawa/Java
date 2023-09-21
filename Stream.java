c0cd474556332364a72c717ce561ffcae0503db6
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-09-21 00:49:03 +0000

    リストの作成

---
+++ Stream.java
@@ -0,0 +1,17 @@
+package test55;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamSample1 {
+  public static void main(String[] args){
+    var date = List.of("yamamoto","kis","sugiyama");
+    var result = new ArrayList<String>();
+    for(var s : date){
+      if(s.length()>=5){
+        result.add(s);
+      }
+    }
+    System.out.println(result);
+  }
+}
