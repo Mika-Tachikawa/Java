7299d1b0512bb4618b20e72dda315d0f5ab19979
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-09-22 23:32:55 +0000

    Streamファイルの記述修正(varなしに修正)

--- Stream.java
+++ Stream.java
@@ -1,14 +1,36 @@
+//package test55;
+
+//import java.util.ArrayList;
+//import java.util.List;
+
+//public class Stream {
+  //public static void main(String[] args){
+    //var date = List.of("yamamoto","kis","sugiyama");
+    //var result = new ArrayList<String>();
+    //for(var s : date){
+      //if(s.length()>=5){
+        //result.add(s);
+      //}
+    //}
+    //System.out.println(result);
+  //}
+//}
+
+
+
+
 package test55;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class StreamSample1 {
+public class Stream {
   public static void main(String[] args){
-    var date = List.of("yamamoto","kis","sugiyama");
-    var result = new ArrayList<String>();
-    for(var s : date){
-      if(s.length()>=5){
+    List<String> date = Arrays.asList("yamamoto", "kis", "sugiyama");
+    List<String> result = new ArrayList<String>();
+    for(String s : date){
+      if(s.length() >= 5){
         result.add(s);
       }
     }
