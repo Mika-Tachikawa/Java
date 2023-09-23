2bfecba5c48e8b8a80dd5bf29b1d05e56d62d11a
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-09-23 00:06:35 +0000

    InstanceMethodファイルの修正（varなし）

--- InstanceMethod.java
+++ InstanceMethod.java
@@ -1,15 +1,37 @@
 package test55;
 
 public class InstanceMethod {
-  record Student(String name, int englishScore, int mathScore){}
-  
-  public static void main(String[] args){
-    var kis = new Student("kis",60,80);
-    var a = average(kis);
-    System.out.println("平均点は%d点です".formatted(a));
+  public static class Student {
+    private String name;
+    private int englishScore;
+    private int mathScore;
+
+    public Student(String name, int englishScore, int mathScore) {
+      this.name = name;
+      this.englishScore = englishScore;
+      this.mathScore = mathScore;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public int getEnglishScore() {
+      return englishScore;
+    }
+
+    public int getMathScore() {
+      return mathScore;
+    }
   }
-  
-  static int average(Student s){
-    return(s.englishScore()+s.mathScore())/2;
+
+  public static void main(String[] args) {
+    Student kis = new Student("kis", 60, 80);
+    int a = average(kis);
+    System.out.println("平均点は" + a + "点です");
+  }
+
+  static int average(Student s) {
+    return (s.getEnglishScore() + s.getMathScore()) / 2;
   }
 }
