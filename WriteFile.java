6658fb3c70ae1b1da17265e1e7bdd15139ff6880
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-09-26 05:36:03 +0000

    Paths.getメソッドへ記述修正

--- WriteFile.java
+++ WriteFile.java
@@ -3,11 +3,12 @@ package test55;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class WriteFile {
   public static void main(String[] args) throws IOException {
     String message = "test\nmessage\n";
-    Path p = Path.of("test.txt");
+    Path p = Paths.get("test.txt");
     Files.write(p, message.getBytes());
   }
 }
