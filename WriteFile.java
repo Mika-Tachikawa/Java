dead736f6c00c8ef880c4347b3682bf25439b9d7
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-09-26 05:33:18 +0000

    varを使わない記述に周淮英

--- WriteFile.java
+++ WriteFile.java
@@ -5,12 +5,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class WriteFile {
-  public static void main(String[] args)throws IOException{
-    var message = ""
-      test
-      message
-      "";
-    var p = Path.of("test.txt");
-    Files.writeString(p,message);
+  public static void main(String[] args) throws IOException {
+    String message = "test\nmessage\n";
+    Path p = Path.of("test.txt");
+    Files.write(p, message.getBytes());
   }
 }
