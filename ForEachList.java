27b2abb9833506728a276ac3a36613bc49e759d0
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-09-29 03:15:35 +0000

    Java8向けの記述に修正

--- ForEachList.java
+++ ForEachList.java
@@ -5,11 +5,9 @@ import java.util.List;
 public class ForEachList {
 
   public static void main(String[] args) {
-    var strs = List.of("apple","banana","grape");
-    for (int i = 0; i < strs.size(); i++) {
-      var str = strs.get(i);
+    List<String> strs = List.of("apple", "banana", "grape");
+    for (String str : strs) {
       System.out.println(str);
     }
   }
-  
 }
