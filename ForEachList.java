d1e05e64bbf45e3db5db68b60c99ef2a4c6860b4
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-09-29 03:19:09 +0000

    Listofの記述不備の修正

--- ForEachList.java
+++ ForEachList.java
@@ -1,11 +1,12 @@
 package test55;
 
 import java.util.List;
+import java.util.Arrays;
 
 public class ForEachList {
 
   public static void main(String[] args) {
-    List<String> strs = List.of("apple", "banana", "grape");
+    List<String> strs = Arrays.asList("apple", "banana", "grape");
     for (String str : strs) {
       System.out.println(str);
     }
