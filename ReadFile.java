112a06c0af3cd3a91454b1383f755e7f91cb0e37
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-10-02 04:53:45 +0000

    javaのバージョンに合わせた記述に修正

--- ReadFile.java
+++ ReadFile.java
@@ -1,18 +1,25 @@
 package test55;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 public class ReadFile {
-  public static void main(String[] args) throws IOException {
-    try {
-      var p = Path.of("test.txta");
-      String s = Files.readString(p);
-      System.out.println(s);
-    }catch (NoSuchFileException e) {
-      System.out.println("ファイルが見つかりません:"+e.getFile());
+    public static void main(String[] args) {
+        try {
+            Path p = Paths.get("test.txta");
+            BufferedReader reader = Files.newBufferedReader(p);
+            String s = reader.lines().collect(Collectors.joining("\n"));
+            System.out.println(s);
+            reader.close();
+        } catch (NoSuchFileException e) {
+            System.out.println("ファイルが見つかりません:" + e.getFile());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
 }
