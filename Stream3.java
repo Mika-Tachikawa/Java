05347553673f017ac32d00ab935a761faa392f82
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-10-02 04:30:00 +0000

    varのない記述に修正

--- Stream3.java
+++ Stream3.java
@@ -1,18 +1,16 @@
 package test55;
 
-inport java.util.List;
+import java.util.Arrays;
+import java.util.List;
 
 public class Stream3 {
-  public static void main(String[] args) {
-    var date = List.of("yamamoto","kis","sugiyama");
-    
-    var result = 0;
-    for (var s : date) {
-      if (s.length() >= 5) {
-        result++;
-      }
-      
+    public static void main(String[] args) {
+        List<String> date = Arrays.asList("yamamoto", "kis", "sugiyama");
+
+        long result = date.stream()
+                .filter(s -> s.length() >= 5)
+                .count();
+
+        System.out.println(result);
     }
-    System.out.println(result);
-  } 
 }
