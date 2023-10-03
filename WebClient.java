70585800e53f49d114e2553cc340b6efad35046e
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-10-03 09:51:31 +0000

    varのない記述に修正

--- WebClient.java
+++ WebClient.java
@@ -1,31 +1,48 @@
 package test55;
 
-import java.io.BuffereReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
 public class WebClient {
-  
-  public static void main(String[] args) throws
-IOException {
-    var domain = "example.com";
-    try (var soc = new Socket(domain,80);
-      var pw = new
-PrintWriter(soc.getOutputStream());
-      var isr = new
-InputStreamReader(soc.getInputStream());
-      var bur = new BufferedReader(isr))
-    {
-      pw.println("GET/index.html HTTP/1.1");
-      pw.println("Host:"+ domain);
-      pw.println();
-      pw.flush();
-      bur.lines()
-        .limit(18)
-        .forEach(System.out::println);
+
+    public static void main(String[] args) throws IOException {
+        String domain = "example.com";
+        Socket socket = null;
+        PrintWriter pw = null;
+        InputStreamReader isr = null;
+        BufferedReader bur = null;
+
+        try {
+            socket = new Socket(domain, 80);
+            pw = new PrintWriter(socket.getOutputStream());
+            isr = new InputStreamReader(socket.getInputStream());
+            bur = new BufferedReader(isr);
+
+            pw.println("GET /index.html HTTP/1.1");
+            pw.println("Host: " + domain);
+            pw.println();
+            pw.flush();
+
+            bur.lines()
+                    .limit(18)
+                    .forEach(System.out::println);
+        } finally {
+            if (bur != null) {
+                bur.close();
+            }
+            if (isr != null) {
+                isr.close();
+            }
+            if (pw != null) {
+                pw.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+        }
     }
-  }
-  
+
 }
