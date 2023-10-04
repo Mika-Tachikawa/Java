e596cdf5a7770bb40ba0d6cbef223c8c85b0eee3
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-10-04 05:41:26 +0000

    Javaのバージョンに合わせた記述に修正

--- RemoveDuplicate.java
+++ RemoveDuplicate.java
@@ -1,18 +1,18 @@
 package test55;
 
 public class RemoveDuplicate {
-  public static void main(String[] args) {
-    var date = "abcccbaabcc";
-    var builder = new StringBuilder();
-    for (int i = 0; i < date.length(); i++) {
-      char ch = date.charAt(i);
-      if (i > 0 && ch == date.charAt(i-1)) {
-        continue;
-      }
-      builder.append(ch);
+    public static void main(String[] args) {
+        String date = "abcccbaabcc";
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < date.length(); i++) {
+            char ch = date.charAt(i);
+            if (i > 0 && ch == date.charAt(i - 1)) {
+                continue;
+            }
+            builder.append(ch);
+        }
+        String result = builder.toString();
+        System.out.println(date);
+        System.out.println(result);
     }
-    var result = builder.toString();
-    System.out.println(date);
-    System.out.println(result);
-  }
 }
