d9263b012122d49e71f6da08b692a7d3e7e85e70
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-10-04 05:39:01 +0000

    重複データを取り除く記述練習

--- RemoveDuplicate.java
+++ RemoveDuplicate.java
@@ -5,9 +5,14 @@ public class RemoveDuplicate {
     var date = "abcccbaabcc";
     var builder = new StringBuilder();
     for (int i = 0; i < date.length(); i++) {
-      continue;
+      char ch = date.charAt(i);
+      if (i > 0 && ch == date.charAt(i-1)) {
+        continue;
+      }
+      builder.append(ch);
     }
-    builder.append(ch);
+    var result = builder.toString();
+    System.out.println(date);
+    System.out.println(result);
   }
-  var 
 }
