da8707f394a6e73aaed53e86c7ec48b5114081b1
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-10-05 06:03:04 +0000

    Javaのバージョンに合わせた記述修正

--- RunLengthCompression.java
+++ RunLengthCompression.java
@@ -2,37 +2,37 @@ package test55;
 
 public class RunLengthCompression {
   public static void main(String[] args) {
-    final var COUNTER_BASE = -1;
-    var date = "abbcccbaaabccccccccccccddd";
-    
-    var count = COUNTER_BASE;
+    final int COUNTER_BASE = -1;
+    String data = "abbcccbaaabccccccccccccddd";
+
+    int count = COUNTER_BASE;
     char prev = 0;
-    var builder = new StringBuilder();
-    for (var ch : data.toCharArray()) {
+    StringBuilder builder = new StringBuilder();
+    for (char ch : data.toCharArray()) {
       if (prev == ch) {
-        //文字が同じだった場合
+        // 文字が同じだった場合
         count++;
-        if(count == 9){
+        if (count == 9) {
           builder.append('9');
           count = COUNTER_BASE;
           prev = 0;
         }
-      }else {
-        //文字が違った時
+      } else {
+        // 文字が違った時
         if (count >= 0) {
-          builder.append((char)('0'+count));
+          builder.append((char) ('0' + count));
           count = COUNTER_BASE;
         }
         builder.append(ch);
         prev = ch;
       }
     }
-    //最後の文字が連続していた場合数字を出力
-    if (count >= 0){
-      builder.append((char)('0'+count));
+    // 最後の文字が連続していた場合数字を出力
+    if (count >= 0) {
+      builder.append((char) ('0' + count));
     }
-    var result = builder.toString();
-    System.out.println(date);
+    String result = builder.toString();
+    System.out.println(data);
     System.out.println(result);
   }
 }
