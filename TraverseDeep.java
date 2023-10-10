a3a975aef22535bde39465534fea003a7aa29619
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-10-10 05:47:10 +0000

    Javaのバージョンに合わせた修正

--- TraverseDeep.java
+++ TraverseDeep.java
@@ -1,39 +1,38 @@
 package test55;
 
 public class TraverseDeep {
-  public static void main(String[] args){
-    int[][] map = {
-      {1,1,1,1,1,1,1},
-      {1,0,1,0,0,0,1},
-      {1,0,0,0,1,1,1},
-      {1,0,1,0,0,2,1},
-      {1,1,1,1,1,1,1},
+    static int[][] map = {
+            {1, 1, 1, 1, 1, 1, 1},
+            {1, 0, 1, 0, 0, 0, 1},
+            {1, 0, 0, 0, 1, 1, 1},
+            {1, 0, 1, 0, 0, 2, 1},
+            {1, 1, 1, 1, 1, 1, 1},
     };
-    traverse(map,1,1);
-    char[] ch = {'.','*','G','o'};
-    for (int[] row : map) {
-      for (int cell : row) {
-        System.out.print(ch[cell]);
-      }
-      System.out.println();
+
+    public static void main(String[] args) {
+        traverse(1, 1);
+        char[] ch = {'.', '*', 'G', 'o'};
+        for (int[] row : map) {
+            for (int cell : row) {
+                System.out.print(ch[cell]);
+            }
+            System.out.println();
+        }
     }
-  }
-  
-  static boolean traverse() {
-    switch (map[curY][curX]) {
-      case 0: break;
-      case 2: return true;
-      default: return false;
+
+    static boolean traverse(int curX, int curY) {
+        if (map[curY][curX] == 0) {
+            map[curY][curX] = 3;
+            if (traverse(curX + 1, curY) ||
+                traverse(curX - 1, curY) ||
+                traverse(curX, curY + 1) ||
+                traverse(curX, curY - 1)) {
+                return true;
+            }
+            map[curY][curX] = 0;
+        } else if (map[curY][curX] == 2) {
+            return true;
+        }
+        return false;
     }
-    map[curY][curX] = 3;
-    if (traverse(map,curX+1,curY])||
-      traverse(map,curX-1,curY])||
-      traverse(map,curX,curY+1])||
-      traverse(map,curX,curY-1])||
-      return true;
-    )
-    map[curY][curX] = 0;
-    return false;
-  }
-  
 }
