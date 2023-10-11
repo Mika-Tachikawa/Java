f5e1c705a8e3a7b4ab5a6fc47e8265cdb9424412
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-10-11 04:20:25 +0000

    Javaのバージョンに合わせた記述に修正

--- Class.java
+++ Class.java
@@ -1,10 +1,40 @@
 package test55;
 
 public class Class {
-  record Student(String name, int score) {}
-  
+  static class Student {
+    private String name;
+    private int score;
+
+    public Student(String name, int score) {
+      this.name = name;
+      this.score = score;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public int getScore() {
+      return score;
+    }
+
+    @Override
+    public String toString() {
+      return "Student{" +
+             "name='" + name + '\'' +
+             ", score=" + score +
+             '}';
+    }
+  }
+
   public static void main(String[] args) {
-    var s = new Student("kis",89);
+    Student s = new Student("kis", 89);
     System.out.println(s);
   }
 }
+この修正では、通常のクラスStudentを使用し、コンストラクタ、アクセサーメソッド、toStringメソッドを追加して、同様の機能を実現しています。
+
+
+
+
+
