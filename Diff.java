3a53d9c5b7b00974291324dd5e22d3b305f407a5
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-10-14 08:03:43 +0000

    誤記修正

--- Diff.java
+++ Diff.java
@@ -13,7 +13,7 @@ public class Diff {
         Graphics2D g = img.createGraphics();
         g.clearRect(0, 0, 600, 400);
         drawImage(g, lineImage(), 10, 10, f);
-        drawImage(g, rectImage(), 300, 80, f);
+        drawImage(g, rectImage(), 300, 80, f);  // 正しいメソッド名 "rectImage()" を使用
         JLabel label = new JLabel(new ImageIcon(img));
         f.add(label);
         f.pack();
@@ -21,6 +21,13 @@ public class Diff {
     }
 
     static BufferedImage lineImage() {
+        BufferedImage image = new BufferedImage(250, 200, BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics = image.createGraphics();
+        graphics.drawRect(10, 10, 220, 180);
+        return image;
+    }
+
+    static BufferedImage rectImage() {  // メソッド名を "rectImage()" に修正
         BufferedImage image = new BufferedImage(250, 200, BufferedImage.TYPE_INT_RGB);
         Graphics2D graphics = image.createGraphics();
         graphics.drawRect(10, 10, 220, 180);
